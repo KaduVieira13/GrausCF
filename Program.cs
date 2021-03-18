632199e9ef9423189c0468c0ec35c3aee39a412a
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-03-18 09:34:24 -0300

    Conversor de temperatura finalizado

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string temperatura;
+            double fahrenheit;
+            double celsius;
+
+            Console.WriteLine ("--- Conversor de Temperatura --- \n");
+            Console.Write("Digite o valor em Fharenheit para conversão: ");
+            temperatura = Console.ReadLine();
+            Console.WriteLine();
+            fahrenheit = Convert.ToDouble(temperatura);
+            celsius = (fahrenheit-32 )  / 1.8;
+            Console.WriteLine($"A temperatura convertida para Celsius é: {celsius:N1}");
+            Console.WriteLine("Digite ENTER para encerrar... ");
+            Console.ReadKey();
+            
+
+        }
+    }
+}
